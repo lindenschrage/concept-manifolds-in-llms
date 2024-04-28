7a2ea215cd2f22cbd608d96990966f2273067c11
Linden Schrage <lindenws@gmail.com>
2024-04-27 23:34:18 -0400

    add

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import random
 from utils import get_embedding_dict, dict_to_json, compute_geometry, process_geometry, convert_to_serializable, sample_tensors_from_dict
 
 thresholds = {3: 'top_3_words', 20: 'top_20_words', 100: 'top_100_words'}
-sample_size = 60
+sample_size = 50
 
 access_token = "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"
 llama_tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token=access_token)
@@ -27,7 +27,6 @@ with open(input_filepath, 'r') as file:
     inputs_dict = json.load(file)
 
 ## GENERATE EMBEDDINGS
-
 toplayerdict = get_embedding_dict(thresholds, inputs_dict, llama_model, llama_tokenizer)
 
 new_data = sample_tensors_from_dict(toplayerdict, sample_size)
