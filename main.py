f4504a36a67c99a0a7471a094f1b75e6033563d9
Linden Schrage <lindenws@gmail.com>
2024-04-30 15:59:59 -0400

    add

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import matplotlib.pyplot as plt
 
 thresholds = {5: 'top_5_words', 100: 'top_100_words', 300: 'top_300_words'}
 results = {key: [] for key in thresholds.values()}
-
-sample_size = 76
+SAMP_SIZE = 76
+LOOPS = 100
 
 access_token = "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"
 llama_tokenizer = LlamaTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token=access_token)
@@ -35,8 +35,9 @@ with open(input_filepath, 'r') as file:
 ## GENERATE EMBEDDINGS
 toplayerdict = get_embedding_dict(thresholds, inputs_dict, llama_model, llama_tokenizer)
 
-for r in range(100):
-    new_data = sample_tensors_from_dict(toplayerdict, sample_size)
+
+for r in range(LOOPS):
+    new_data = sample_tensors_from_dict(toplayerdict, SAMP_SIZE)
 
     ## STORE AS JSON FILE
     data_dict = dict_to_json(new_data)
